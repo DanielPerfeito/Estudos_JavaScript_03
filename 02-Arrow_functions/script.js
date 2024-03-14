52448f314d7cb4b8e8781e023a06a77acc7b551b
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2024-03-14 14:39:56 -0300

    Arrow Functions

---
+++ 02-Arrow_functions/script.js
@@ -0,0 +1,18 @@
+//ARROW FUNCTIONS
+
+/*As arrow functions são uma sintaxe mais concisa e simples para criar funções em JavaScript. sua sintaxe normalmente segue da seguinte maneira: () => {}.
+Aqui estão alguns exemplos simples de como usar arrow functions:*/
+
+//1-Arrow function que retorna a soma de dois números:
+const soma = (a,b) => a + b;
+
+console.log(soma(4,2));
+
+//2-Arrow function que retorna o dobro de um numero:
+
+const dobro = (num) => num * 2;
+
+console.log(dobro(7));
+
+
+
